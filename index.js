0a43de2a5c1a73c55a4709328739dee014bd5e0e
divamarketingvt-droid <divamarketingvt@gmail.com>
2026-05-07 14:47:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -63,15 +63,18 @@ const refreshZohoToken = async () => {
 
 // ==========================================
 // MIDDLEWARE
+"https://backend-vtech.onrender.com",
 // ==========================================
 const allowedOrigins = [
-  "https://backend-vtech.onrender.com",
+  
   "http://localhost:3000",
   "http://localhost:5000",
   "http://127.0.0.1:3000",
   "http://127.0.0.1:5000",
   "http://127.0.0.1:5500",
   "http://localhost:5500",
+  "https://violet-wasp-111896.hostingersite.com",
+
   "null",
   undefined,
 ];
